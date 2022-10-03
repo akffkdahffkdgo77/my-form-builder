7eb42abae81fa4726ac9fa64945a15f604918b02
김가영 <namiein@naver.com>
2022-10-03 15:39:07 +0900

    mod : browserrouter -> hashrouter로 변경

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Home from 'pages/Home';
 
 function Router() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 export default Router;
