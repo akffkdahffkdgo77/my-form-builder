4063ca89d463d16c25dde522d4e68323fd17e35c
김가영 <namiein@naver.com>
2022-10-03 15:38:43 +0900

    mod : react 18로 업데이트 및 사용 안하는 코드 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from 'app/App';
+import 'styles/index.css';
 
-const queryClient = new QueryClient();
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-ReactDOM.render(
+root.render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+        <App />
+    </React.StrictMode>
 );
