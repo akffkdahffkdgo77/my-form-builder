666f067c9addbef741ed00b7b8f7036ac09ae7ed
김가영 <namiein@naver.com>
2022-10-07 22:46:03 +0900

    mod : min height은 300px으로만 설정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -52,7 +52,7 @@ export default function Home() {
         <main className="bg-[#F6F6F6] w-full min-h-screen flex justify-center items-center flex-col pt-10 font-mono p-5 ">
             <h1 className="text-5xl font-bold underline text-[#8785A2] mb-10">My Form Builder</h1>
             <h2 className="text-3xl text-[#FFC7C7] mb-10">Simple HTML Form Builder</h2>
-            <div className="w-full min-h-[300px] lg:min-h-[500px] flex justify-center items-start gap-5 flex-wrap xl:flex-nowrap">
+            <div className="w-full min-h-[300px] flex justify-center items-start gap-5 flex-wrap xl:flex-nowrap">
                 <Layout list={list} setList={setList} />
                 <Creator
                     name={name}
